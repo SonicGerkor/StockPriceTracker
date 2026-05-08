58df9ad538f4f3315ab5d3ec35adfcfbc1ee1890
German Battiston <ger.battiston@gmail.com>
2026-05-08 12:35:06 -0300

    Added git ignore

---
+++ StockPriceTracker/StockPriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPriceTrackerApp.swift
+//  StockPriceTracker
+//
+//  Created by German Battiston on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct StockPriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
